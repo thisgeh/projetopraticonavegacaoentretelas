5c3962fdf16d2b453d8d0a44bc27498c0e320a68
thisgeh <santosgeisa413@gmail.com>
2026-03-13 23:59:20 -0300

    feat: navegacao entre telas vHome

---
+++ app/src/main/java/thisgeh/com/github/projetopraticonavegacaoentretelas/MainActivity.kt
@@ -0,0 +1,50 @@
+package thisgeh.com.github.projetopraticonavegacaoentretelas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import thisgeh.com.github.projetopraticonavegacaoentretelas.screens.LoginScreen
+import thisgeh.com.github.projetopraticonavegacaoentretelas.screens.MenuScreen
+import thisgeh.com.github.projetopraticonavegacaoentretelas.screens.PedidosScreen
+import thisgeh.com.github.projetopraticonavegacaoentretelas.screens.PerfilScreen
+import thisgeh.com.github.projetopraticonavegacaoentretelas.ui.theme.ProjetopraticonavegacaoentretelasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjetopraticonavegacaoentretelasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                    ){
+                        composable(route = "login") {
+                            LoginScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "menu"){
+                            MenuScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "pedidos"){
+                            PedidosScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "perfil"){
+                            PerfilScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/thisgeh/com/github/projetopraticonavegacaoentretelas/screens/LoginScreen.kt
@@ -0,0 +1,45 @@
+package thisgeh.com.github.projetopraticonavegacaoentretelas.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(modifier: Modifier, navController: NavController){
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xff363636))
+            .padding(32.dp)
+    ){
+        Text(
+            text = "LOGIN",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Button(
+            onClick = {navController.navigate(route = "menu")},
+            colors = ButtonDefaults.buttonColors(Color.White),
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(
+                text = "Entrar",
+                fontSize = 20.sp,
+                color = Color(0xff363636)
+            )
+        }
+    }
+}
